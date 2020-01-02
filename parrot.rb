51c9c15590b880aba1ca85855490d8357acb2040
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-01-02 07:26:43 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,7 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 def parrot(phrase ="Squawk!")
-  puts "What does the parrot say? #{phrase}"
+  puts ""What does the parrot say? "#{phrase}"
 end
 
 parrot()
